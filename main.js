81a25d4f2b68ecea07f36b474b2fb074ad680d17
issueyh <issueyh12921@gmail.com>
2021-07-16 15:52:07 +0800

    fix: innerHTML without itemName

--- main.js
+++ main.js
@@ -1,49 +1,49 @@
-const engineer = [
-    {
-        id: 1,
-        name: "林永剛",
-        avatar: "./image/muscle.jpg",
-        phone: "0925-090-801",
-        email: "move1214@hotmail.com"
-    },
-    {
-        id: 2,
-        name: "張文千",
-        avatar: "./image/muscle.jpg",
-        phone: "0937-182-391",
-        email: "mika.cwc.tt@gmail.com"
-    },
-    {
-        id: 3,
-        name: "林佳慶",
-        avatar: "./image/muscle.jpg",
-        phone: "0927-002-205",
-        email: "daniel650807@hotmail.com"
-    },
-    {
-        id: 4,
-        name: "顏銘辰",
-        avatar: "./image/muscle.jpg",
-        phone: "0918-153-575",
-        email: "n49517028@gmail.com"
-    },
-    {
-        id: 5,
-        name: "邱炳盛",
-        avatar: "./image/muscle.jpg",
-        phone: "0989-203-216",
-        email: "n49517044@gmail.com"
-    },
-    {
-        id: 6,
-        name: "葉詠展",
-        avatar: "./image/詠展jpg",
-        phone: "0958-206-320",
-        email: "ya66321@gmail.com"
-    }
-]
-// const engineer = require('./engineer.json')
+// const engineer = [
+//     {
+//         id: 1,
+//         name: "林永剛",
+//         avatar: "./image/muscle.jpg",
+//         phone: "0925-090-801",
+//         email: "move1214@hotmail.com"
+//     },
+//     {
+//         id: 2,
+//         name: "張文千",
+//         avatar: "./image/muscle.jpg",
+//         phone: "0937-182-391",
+//         email: "mika.cwc.tt@gmail.com"
+//     },
+//     {
+//         id: 3,
+//         name: "林佳慶",
+//         avatar: "./image/muscle.jpg",
+//         phone: "0927-002-205",
+//         email: "daniel650807@hotmail.com"
+//     },
+//     {
+//         id: 4,
+//         name: "顏銘辰",
+//         avatar: "./image/muscle.jpg",
+//         phone: "0918-153-575",
+//         email: "n49517028@gmail.com"
+//     },
+//     {
+//         id: 5,
+//         name: "邱炳盛",
+//         avatar: "./image/muscle.jpg",
+//         phone: "0989-203-216",
+//         email: "n49517044@gmail.com"
+//     },
+//     {
+//         id: 6,
+//         name: "葉詠展",
+//         avatar: "./image/詠展jpg",
+//         phone: "0958-206-320",
+//         email: "ya66321@gmail.com"
+//     }
+// ]
 
+const engineer = []
 const dataPanel = document.querySelector("#data-panel")
 
 function renderEngineerList(data) {
@@ -69,25 +69,23 @@ function showNameCardModel(id) {
     const modalImage = document.querySelector("#member-modal-image")
     const modalPhone = document.querySelector("#member-modal-phone")
     const modalEmail = document.querySelector("#member-modal-email")
-    // const { id, name, avatar, phone, email } = engineer
-    // const data = engineer.data
-    // const nameCard = document.querySelector("#nameCard")
-    // const id = engineer.results
-    // nameCard.findById(id).then(data => {
-    // if (nameCard.id === id) {
-        // axios.get(id).then((data) => {
-            const data = engineer.find((item) => item.id === id)
-            modalTitle.innerHTML = `${data.name}`
-            modalPhone.innerHTML = `phone: ${data.phone}`
-            modalEmail.innerHTML = `email: ${data.email}`
-            modalImage.innerHTML = `
-            <img src="${data.avatar}" alt="avatar">`
-        // })
-        
-    // }
-    // })
+    fetch("engineer.json")
+        .then((data) => data.json('engineer'))
+        .then((result) => {
+            // console.log(result)
+            const member = result.engineer.map((item) => {
+                if (item.id == id) {
+                    modalTitle.innerHTML = `${item.name}`
+                    // console.log(typeof(item.name))
+                    modalPhone.innerHTML = `phone: ${item.phone}`
+                    modalEmail.innerHTML = `email: ${item.email}`
+                    modalImage.innerHTML = `
+                    <img src="${item.avatar}" alt="avatar" class="img-fluid" style="border-radius: 50%;">`
+                }
+            })
+            return member
+        })
 }
-
 
 dataPanel.addEventListener('click', function onPanelClicked(event) {
     if (event.target.matches('.btn-show-member')) {
@@ -96,4 +94,11 @@ dataPanel.addEventListener('click', function onPanelClicked(event) {
     }
 })
 
-renderEngineerList(engineer)
+fetch("engineer.json")
+    .then((data) => data.json('engineer'))
+    .then((result) => {
+        // console.log(result)
+        engineer.push(...result.engineer)
+        renderEngineerList(engineer)
+    })
+    .catch((error) => console.log(error))
